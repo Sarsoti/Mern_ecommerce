8fd17b896952a56883327d14636e5dd29e64127c
Sarsoti <subedisaraswati8@gmail.com>
2021-05-10 21:50:09 +0545

    frontend and little backend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+
+
+const Header = () => {
+    return (
+      <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Container>
+          <LinkContainer to = "/">
+            <Navbar.Brand>Dhakatopi</Navbar.Brand>
+          </LinkContainer>
+          
+            <Nav className="ml-auto">
+              <LinkContainer to = '/cart'>
+              <Nav.Link> 
+                 <i className ="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              </LinkContainer>
+               <LinkContainer to = '/login'>
+               <Nav.Link> <i className ="fas fa-user"></i>Sign in</Nav.Link>
+               </LinkContainer>
+              
+            </Nav>
+         
+        </Container>
+      </Navbar>
+    )
+}
+
+export default Header
+
